3eaf0ebc9630f66d22d554d49985493c29dc7af5
Nitin <26771985+nitinx@users.noreply.github.com>
2020-01-15 21:22:16 +0530

    Data Quality + Review comments
    Completed data quality checks and addressed review comments

--- operators/data_quality.py
+++ operators/data_quality.py
@@ -2,9 +2,9 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
-class LoadDimensionOperator(BaseOperator):
+class DataQualityOperator(BaseOperator):
 
-    ui_color = '#80BD9E'
+    ui_color = '#89DA59'
 
     @apply_defaults
     def __init__(self,
@@ -12,28 +12,31 @@ class LoadDimensionOperator(BaseOperator):
                  # Example:
                  # conn_id = your-connection-name
                  redshift_conn_id="",
-                 sql="",
-                 mode="",
-                 table="",
+                 dq_checks="",                 
                  *args, **kwargs):
 
-        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
         self.redshift_conn_id = redshift_conn_id
-        self.sql = sql
-        self.mode = mode
-        self.table = table
-        
+        self.dq_checks = dq_checks
 
     def execute(self, context):
-        #aws_hook = AwsHook(self.aws_credentials_id)
-        #credentials = aws_hook.get_credentials()
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         
         self.log.info("Starting Stage to Dimension Load")
-        if self.mode == "delete":
-            redshift.run("DELETE FROM {}".format(self.table))
-        redshift.run(self.sql)
-        
+        for check in self.dq_checks:
+            sql = check.get('check_sql')
+            exp_result = check.get('expected_result')
+ 
+            records = redshift.get_records(sql)[0]
+ 
+            if exp_result != records[0]:
+                error_count += 1
+                failing_tests.append(sql)
+ 
+        if error_count > 0:
+            self.log.info('Tests failed')
+            self.log.info(failing_tests)
+            raise ValueError('Data quality check failed')       
--- udac_example_dag.py
+++ udac_example_dag.py
@@ -11,17 +11,30 @@ from helpers import SqlQueries
 
 default_args = {
     'owner': 'udacity',
-    'start_date': datetime(2018, 11, 12),
+    'start_date': datetime(2018, 11, 1),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),    
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'catchup': False,
 }
 
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
           #schedule_interval='0 * * * *'
-          #schedule_interval='@monthly',
+          schedule_interval='@hourly',
           max_active_runs=1
         )
-'''
+
+dq_checks_null=[
+    {'check_sql': "SELECT COUNT(*) FROM users WHERE userid is null", 'expected_result': 0},
+    {'check_sql': "SELECT COUNT(*) FROM songs WHERE songid is null", 'expected_result': 0},
+    {'check_sql': "SELECT COUNT(*) FROM artists WHERE artistid is null", 'expected_result': 0}
+]
+
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
@@ -31,8 +44,9 @@ stage_events_to_redshift = StageToRedshiftOperator(
     redshift_conn_id="redshift",
     aws_credentials_id="aws_credentials",
     s3_bucket="udacity-dend",
-    #s3_key="log_data/{execution_date.year}/{execution_date.month}"
-    s3_key="log_data",
+    provide_context=True,
+    s3_key="log_data/{execution_date.year}/{execution_date.month}"
+    #s3_key="log_data",
     region='us-west-2'
 )
 
@@ -59,42 +73,45 @@ load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
     dag=dag,
     redshift_conn_id="redshift",
-    sql=SqlQueries.user_table_insert   
+    sql=SqlQueries.user_table_insert,
+    mode='append',
+    table='users'
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
     dag=dag,
     redshift_conn_id="redshift",
-    sql=SqlQueries.song_table_insert    
+    sql=SqlQueries.song_table_insert,
+    mode='append',
+    table='songs'
 )
-'''
+
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
     dag=dag,
     redshift_conn_id="redshift",
-    sql=SqlQueries.artist_table_insert    
+    sql=SqlQueries.artist_table_insert,
+    mode='append',
+    table='artists'
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
     dag=dag,
     redshift_conn_id="redshift",
-    sql=SqlQueries.time_table_insert    
+    sql=SqlQueries.time_table_insert,
+    mode='append',
+    table='time'
 )
-'''
+
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
-    dag=dag
+    dag=dag,
+    dq_checks=dq_checks_null
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
-
-start_operator >> stage_events_to_redshift
-start_operator >> stage_songs_to_redshift
-stage_events_to_redshift >> end_operator
-stage_songs_to_redshift >> end_operator
-
 
 
 start_operator >> stage_events_to_redshift
@@ -105,6 +122,12 @@ load_songplays_table >> load_user_dimension_table
 load_songplays_table >> load_song_dimension_table
 load_songplays_table >> load_artist_dimension_table
 load_songplays_table >> load_time_dimension_table
+load_user_dimension_table >> end_operator
+load_song_dimension_table >> end_operator
+load_artist_dimension_table >> end_operator
+load_time_dimension_table >> end_operator
+
+'''
 load_user_dimension_table >> run_quality_checks
 load_song_dimension_table >> run_quality_checks
 load_artist_dimension_table >> run_quality_checks
